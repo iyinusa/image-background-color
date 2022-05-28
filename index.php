fc9473203f4bd15ae288b93e769aa7a4498f56d0
I. Kennedy Yinusa <2399466+iyinusa@users.noreply.github.com>
2022-05-28 08:42:54 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+ error_reporting(0);
+/***************************************************************************************
+							Main Using  Class DetectImageColor
+***************************************************************************************/
+ require('detectImageColor.php');
+ 
+ $path = "images3.png";
+
+	$mainDetectImageColor = new DetectImageColor($path);
+
+	//echo $mainDetectImageColor->getColorBackground($mainDetectImageColor->getHexa($path))."<br/>";
+		
+	// var_dump($mainDetectImageColor);
+
+	echo $mainDetectImageColor->getHexa($path);
+   
+?>
